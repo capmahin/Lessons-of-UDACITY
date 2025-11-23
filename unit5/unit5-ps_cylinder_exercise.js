0cca6f990a611189ac8d186cb7f298cda0399be7
capmahin <mahinulhuque1996@gmail.com>
2025-11-23 12:24:52 +0600

    solve

--- unit5/unit5-ps_cylinder_exercise.js
+++ unit5/unit5-ps_cylinder_exercise.js
@@ -28,9 +28,13 @@ function createCylinderFromEnds( material, radiusTop, radiusBottom, top, bottom,
 	openEnded = (openEnded === undefined) ? false : openEnded;
 
 	// Dummy settings, replace with proper code:
-	var length = 100;
-	var cylAxis = new THREE.Vector3(100,100,-100);
-	var center = new THREE.Vector3(-100,100,100);
+	// get cylinder height
+	var cylAxis = new THREE.Vector3();
+	cylAxis.subVectors( bottom, top );
+	var length = cylAxis.length();
+	// get center of cylinder
+	var center = new THREE.Vector3();
+	center.addVectors( top, bottom ).divideScalar(2.0);
 	////////////////////
 
 	var cylGeom = new THREE.CylinderGeometry( radiusTop, radiusBottom, length, segmentsWidth, 1, openEnded );
