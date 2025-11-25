852c641e9c8df4ffef69e40d43d4d8ea2b7771e7
capmahin <mahinulhuque1996@gmail.com>
2025-11-25 23:07:14 +0600

    solve

--- unit7/12-fov-slider.js
+++ unit7/12-fov-slider.js
@@ -22,6 +22,21 @@ function init() {
 	var canvasWidth = 846;
 	var canvasHeight = 494;
 	// For grading the window is fixed in size; here's general code:
+	effectController ={
+		fov40
+	};
+	var gui = new dat.GUI();
+	var element = gui.add( effectController, "fov" ,1.0,179.0);
+	element.name("field of view")
+
+	function render(){
+		var delta = clock.getDelta();
+		cameraControls.update(delta);
+
+		camera.fov = effectController.fov;
+		camera.updateProjectionMatrix();
+		renderer.render(scene, camera);
+	}
 	//var canvasWidth = window.innerWidth;
 	//var canvasHeight = window.innerHeight;
 
